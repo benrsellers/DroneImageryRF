af031c4a526d1f6874d8c7ccd5b0f2e9b563375c
Ben Sellers <benrsellers@gmail.com>
2024-12-16 18:11:01 -0600

    making RF script dynamic to account for different # of classes and class names

---
+++ Step1_PreProcessing.R
@@ -0,0 +1,167 @@
+########################################################################
+
+#Project: Mapping Rangeland Vegetation Attributes to Inform Grazing Management in Moffat County
+#Node: Colorado, Fort Collins
+#Term: Fall 2024
+#Team: Jack Hagenbuch, Mike Russo, Sam Metzger, Ben Sellers 
+#Code Contact: Ben Sellers (benrsellers@gmail.com)
+
+# ----------------------------------------------------------------------
+# Step 2: Drone Raster Calculations and Stacking 
+# Ben Sellers
+
+# Drone_RasterStack.R uses RGB and DSM/DTM layers from processed drone imagery 
+# to calculate vegetation indices and terrain metrics and creates a composite for 
+# use in supervised classification. 
+
+# General scripts steps include:
+#1. Read in all raster data, smooth DSM and DTM to remove NAs and resample RGB 
+#   orthomosaic to have same resolution as DSM/DTM.
+
+#2. Calculate RGB vegetation indices: VDVI, GRRI, NGRDI, and VARI. 
+
+#3. Calculate structural metrics: Canopy height model, Roughness, TPI, and TRI.
+
+#4. Rename raster bands and stack them into a composite raster, write the output to output folder
+
+
+# INPUTS
+# 1: Path to the rgb orthomosaic raster. Band 1 = Red, Band 2 = Green, Band 3 = Blue, Band 4 = Alpha)(rgb_raster_path)
+# 2: Path to digital terrain model raster (dtm_raster_path)
+# 3: Path to digital surface model raster (dsm_raster_path)
+# 4: Path where raster calculations and final raster stack should be written to (output_folder_path)
+
+# OUTPUTS
+# 1. DTM with extrapolated NA values
+# 2. DSM with extrapolated NA Values
+# 3. Resampled RGB orthomosaic
+# 4. Individual RGB vegetation index rasters
+# 5. Terrain raster with bands for TRI, TPI, and Roughness
+# 6. Composite raster with 14 bands
+
+
+########################################################################
+
+# Install necessary packages
+# install.packages("terra")
+
+# Load libraries
+library(terra)
+
+# ----------------------------------------------------------------------
+### Start Fill in Data ###
+rgb_raster_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/tif/PheasantBranch_RGB3cm.tif") # Required
+dtm_raster_path <-NA # Optional. Leave this as N/A if you dont have a dtm
+dsm_raster_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/tif/PheasantBranch_DEM.tif") # Required
+output_folder_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/tif/DroneStack") # Required
+### End Fill in Data ###
+# ----------------------------------------------------------------------
+
+###################################################
+### Step 0: Setup file paths and output folders ###         
+###################################################
+
+# Create the outputs folder
+if (!dir.exists(output_folder_path)) {
+  dir.create(output_folder_path, recursive = TRUE)
+  cat("Folder created successfully at", output_folder_path)
+} else {
+  cat("Folder already exists at", output_folder_path)
+}
+
+##########################################################
+###   Step 1: Fill NAs and Resample RGB orthomosaic    ###
+##########################################################
+
+#Reading in Drone layers 
+if (!is.na(dtm_raster_path)) {
+  drone_dtm <- rast(dtm_raster_path)
+} else {
+  print("No DTM Raster Provided")
+}
+drone_dsm <- rast(dsm_raster_path) #"data/tif/drone_dsm_focal.tif") #This layer was made in Moffat_Random_Forest_Sellers to remove NAs using focal smoothing
+drone_rgb <- rast(rgb_raster_path)#This layer was mad3 in Moffat_Random_Forest_Sellers to resample RGB to the resolution of DSM and DTM (10cm)
+
+# Resample RGB so pixels are same size as DSM/DTM
+drone_rgb <- resample(drone_rgb, drone_dsm,threads=TRUE)
+writeRaster(drone_rgb, file.path(output_folder_path,"drone_rgb10cm.tif"), overwrite = TRUE)
+
+# Fill NA cells in DTM/DSM rasters
+drone_dsm <- focal(drone_dsm, w=9, fun=mean, na.policy="only", na.rm=T)
+writeRaster(drone_dsm, file.path(output_folder_path,"drone_dsm_focal.tif"), overwrite = T)
+if (!is.na(dtm_raster_path)) {
+  drone_dtm <- focal(drone_dtm, w=9, fun=mean, na.policy="only", na.rm=T)
+  writeRaster(drone_dtm, file.path(output_folder_path,"drone_dtm_focal.tif"), overwrite = T)
+} else {
+  print("No DTM Raster Provided")
+}
+
+# Rename Bands
+names(drone_rgb) <- c("red","green", "blue", "alpha")
+if (!is.na(dtm_raster_path)) {
+  names(drone_dtm) <- "dtm"
+} else {
+  print("No DTM Raster Provided")
+}
+names(drone_dsm) <- "dsm"
+###########################################
+###       Step 2: Calculate RGB VIs     ###
+###########################################
+
+
+# Calculating RGB Vegetation Indices
+VDVI = (2*drone_rgb$green-drone_rgb$red - drone_rgb$blue)/(2*drone_rgb$green+drone_rgb$red+drone_rgb$blue)
+writeRaster(VDVI, file.path(output_folder_path,"VDVI.tif"), overwrite = T)
+NGRDI = (drone_rgb$green-drone_rgb$red)/(drone_rgb$green+drone_rgb$red)
+writeRaster(NGRDI, file.path(output_folder_path,"NGRDI.tif"), overwrite = TRUE)
+VARI = (drone_rgb$green-drone_rgb$red)/(drone_rgb$green+drone_rgb$red-drone_rgb$blue)
+writeRaster(VARI, file.path(output_folder_path,"VARI.tif"), overwrite = TRUE)
+GRRI = drone_rgb$green/drone_rgb$red
+writeRaster(GRRI, file.path(output_folder_path,"GRRI.tif"), overwrite = TRUE)
+
+###########################################
+### Step 2: Calculate Structural Indices###
+###########################################
+
+#Create canopy height model by subtracting DTM from DSM
+if (!is.na(dtm_raster_path)) {
+  CHM <- drone_dsm - drone_dtm
+  writeRaster(CHM, file.path(output_folder_path,"drone_CHM.tif"), overwrite = TRUE)
+} else {
+  print("No DTM Raster Provided, can't calc CHM")
+}
+
+# Using terra's built in terrain characteristics function to get TPI, TRI, and Roughness from drone DSM
+terrain_metrics <- terrain(drone_dsm, v=c("TRI", "TPI", "roughness"), neighbors = 8, filename=file.path(output_folder_path,"terrain.tif"))
+
+###########################################
+###     Step 3: Stack  Rasters          ###
+###########################################
+
+# Stack all of the layers together
+
+if (!is.na(dtm_raster_path)) {
+  drone_stack <- c(drone_rgb, drone_dsm, drone_dtm, CHM, terrain_metrics$TRI, terrain_metrics$TPI,
+                   terrain_metrics$roughness, VDVI, NGRDI, VARI, GRRI)
+} else {
+  drone_stack <- c(drone_rgb, drone_dsm, terrain_metrics$TRI, terrain_metrics$TPI,
+                   terrain_metrics$roughness, VDVI, NGRDI, VARI, GRRI)
+  
+}
+
+
+#rename the drone layer names
+if (!is.na(dtm_raster_path)) {
+  names(drone_stack) <- c("r", "g", "b", "alpha", "dsm", "dtm", "chm", "tri", "tpi",
+                          "roughness", "vdvi", "ngrdi", "vari", "grri")
+} else {
+  names(drone_stack) <- c("r", "g", "b", "alpha", "dsm", "tri", "tpi",
+                          "roughness", "vdvi", "ngrdi", "vari", "grri")
+  
+}
+
+#changing all no data values to 0 so that RF can run on raster in next step
+drone_stack[is.na(drone_stack)] <- 0
+
+# Write Drone stack raster
+writeRaster(drone_stack, file.path(output_folder_path,"drone_stack.tif"), overwrite = TRUE)
---
+++ Step2_MapClassification.R
@@ -0,0 +1,392 @@
+########################################################################
+
+#Project: Mapping Rangeland Vegetation Attributes to Inform Grazing Management in Moffat County
+#Node: Colorado, Fort Collins
+#Term: Fall 2024
+#Team: Jack Hagenbuch, Mike Russo, Sam Metzger, Ben Sellers 
+#Code Contact: Ben Sellers (benrsellers@gmail.com)
+
+# ----------------------------------------------------------------------
+# Step 3: Drone Ocular Sampling Random Forest Model 
+# Ben Sellers
+
+# Drone_OcularSamplingRandomForest.R uses ocular training data polygons of
+# plant functional groups (Grass/forbs, Shrubs, Bareground, and Cheatgrass)
+# and creates a random forest model using pixel values extracted from drone imagery. 
+# The model is then projected across the entire imagery area to classify vegetation.
+# Finally, the model is evaluated using validation data from using a user defined
+# training/test split and error metrics are recorded. This script can be run multiple 
+# times to test the influence of model parameters and training data on model outputs.
+
+### Script Information ###
+#1. Read in all shapefile data, assign classes, and merge into one shapefile.
+#2. Split training polygons into calibration/validation sets and extract pixels from drone data for both sets
+#3. Train an RF Model
+#4. Run RF Model on the entire Camblin Ranch drone data stack
+#5. Calculate error metrics from classification maps
+#6. Compare field plot data to classified maps and create linear regressions to assess accuracy
+
+# General scripts steps include:
+# 1. Read in all ocular training data, assign classes to each for classification,
+#    and merge them into a single shapefile. 
+
+# 2. Split training polygons into a calibration and validation set. The worlflow currently
+#    runs the split on the number of polygons given for each class. 
+
+# 3. Train the RF Model by supplying it X data and Y data. This step also returns 
+#    the out of bag error from the model, correlation analysis, and an importance plot for each band. 
+
+# 4. Run the Random Forest Model on the raster stack provided to classify pixels and output a map.
+
+# 5. Calculate error metrics (overall accuracy and Kappa) using validation data and export a confusion matrix.
+
+# 6. Write all model inputs andmetrics to ModelRuns.csv so to keep track of model runs and performance.
+
+# INPUTS
+# 1: Path to the folder where shrub training data polygons are stored (shrub_path)
+# 2: Path to the folder where grass/forb training data polygons are stored (grassforb_path)
+# 3: Path to the folder where bare ground training data polygons are stored (bareground_path)
+# 4: Path to the folder where cheatgrass training data polygons are stored (cheatgrass_path)
+# 5: Path to drone_stack raster, created in Drone_RasterStack.R (drone_stack_path)
+# 6: Path to the folder where model outputs are stored - script creates folder if it doesn't already exist (classification_out_path)
+# 7: Random forest parameter mtry - determines how many variables to try at once in creating model (num_mtry) 
+# 8: Random forest parameter trees - determines the number of trees used in creating model (num_trees)
+# 9: Parameter to determine the proportion of training vs test data. EX: 0.8 = 80% training/test split (cal_val_split)
+# OUTPUTS
+# 1. ModelRuns folder containing subfolders for each random forest model
+# 2. ModelRuns.csv containing spreadsheet of each model run's parameters and performance
+# 3. ConfusionMatrix_***.png containing the confusion matrix from validation data compairson
+# 4. "shp/calibration.shp" shapefile containing polygons used for model training
+# 5. "shp/validation.shp" shapefile containing polygons used for model testing
+
+
+########################################################################
+
+# This script was built using R 4.4.1 
+
+### PACKAGES ###
+
+
+# Install necessary packages
+# install.packages("terra")
+# install.packages("sf")
+# install.packages("randomForest")
+# install.packages("caret")
+# install.packages("dplyr")
+
+# Load libraries
+library(terra)
+library(sf)
+library(dplyr)
+library(randomForest)
+library(ggplot2)
+library(tidyr)
+library(caret)
+
+# ----------------------------------------------------------------------
+### Start Fill in Data ###
+class_count = 7 # Number of classes 
+class_names = c("EmergentVeg", "LightBrownVeg", "LightGreenVeg", "Phrag", "Rice", "Shrubs", "Water") #Names of classes
+training_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/shp/PB_training") #Path to training data folder
+drone_stack_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/tif/DroneStack/drone_stack.tif")
+classification_out_path <- file.path("E:/CoveringGround/repos/PB_data/overview_rgb/tif/ModelOutputs")
+num_mtry <- 7
+num_trees <- 50
+cal_val_split <- .8
+### End Fill in Data ###
+# ----------------------------------------------------------------------
+
+
+###########################################################
+### Step 0: SETUP setting file paths and output folders ###         
+###########################################################
+
+# Create folder for classification outputs, unless it already exists
+if (!dir.exists(classification_out_path)) {
+  dir.create(classification_out_path, recursive = TRUE)
+  cat("Folder created successfully at", classification_out_path)
+} else {
+  cat("Folder already exists at", classification_out_path)
+}
+
+# Variable for todays dat to label RF outputs
+current_date <- format(Sys.Date(), "%Y-%m-%d")
+
+# Setting up variables so that we can run models in a loop and keep track of their run number
+folder_count <- length(list.dirs(classification_out_path, recursive = F))+1
+
+#creating folder for run
+output_foldername <- file.path(classification_out_path, paste0("Run_", folder_count,'_', current_date))
+dir.create(output_foldername, recursive = TRUE)
+
+#create folder for output training data
+calval_shp_path_out <- file.path(output_foldername,"shp")
+dir.create(calval_shp_path_out, recursive = TRUE)
+
+# spreadsheet for logging model runs and corresponding parameters
+modelruns_path <- file.path(classification_out_path, "ModelRuns.csv")
+
+# make new spreadsheet for ModelRuns unless it already exists
+if (!file.exists(modelruns_path)) {
+  # Create a new CSV with sample data (or an empty one)
+  data <- data.frame(Run_num = character(), mtry = numeric(), trees = numeric(), calval_split = numeric(),
+                     Shrub_pix = numeric(), Bare_pix = numeric(), Grass_pix = numeric(), OverallAccuracy = numeric(), Kappa = numeric(), OOB = numeric())
+  write.csv(data, modelruns_path, row.names = FALSE)
+  
+  cat("File created:", modelruns_path, "\n")
+} else {
+  cat("File already exists:", modelruns_path, "\n")
+}
+
+
+#Test
+# Make new spreadsheet for ModelRuns unless it already exists
+if (!file.exists(modelruns_path)) {
+  # Dynamically create column names for each class's pixel count based on class names
+  class_columns <- paste0(class_names, "_pix")
+  
+  # Create a new CSV with sample data (or an empty one) including pixel counts for each class
+  data <- data.frame(
+    Run_num = character(),
+    mtry = numeric(),
+    trees = numeric(),
+    calval_split = numeric(),
+    OverallAccuracy = numeric(),
+    Kappa = numeric(),
+    OOB = numeric(),
+    # Dynamically add the class columns based on class_names
+    matrix(ncol = length(class_names), nrow = 0)  # Empty matrix for class pixel columns
+  )
+  
+  # Convert matrix into appropriate column names
+  colnames(data)[8:(7 + length(class_names))] <- class_columns
+  
+  # Write the empty data frame to CSV
+  write.csv(data, modelruns_path, row.names = FALSE)
+  
+  cat("File created:", modelruns_path, "\n")
+} else {
+  cat("File already exists:", modelruns_path, "\n")
+}
+
+
+#
+# Generate dynamic paths for each class
+training_paths <- lapply(1:class_count, function(i) {
+  file.path(paste0(training_path, "/", i, "_", class_names[i],"TrainingData"))
+})
+training_paths
+################################################
+### Step 1. Merging Training Data Shapefiles ###         
+################################################
+
+
+# Function for reading in all training polygon shapefiles and joining them into one sf polygon
+read_and_merge_training_polygons <- function(filepaths) {
+  #make an empty sf object to store joined shapefiles
+  shapefiles <- NULL
+  
+  #loop over shapefiles in the list of filepaths you supply
+  for (i in filepaths) {
+    shp = st_read(i)
+    if (is.null(shapefiles)) {
+      shapefiles = shp
+    } else {
+      shapefiles <- rbind(shapefiles, shp)
+    }
+  } 
+  return(shapefiles)
+}
+
+read_and_merge_training_polygons <- function(filepaths, class_id) {
+  shapefiles <- do.call(rbind, lapply(filepaths, function(path) {
+    shp <- st_read(path)
+    shp$class <- class_id
+    shp
+  }))
+  return(shapefiles)
+}
+
+# Combine shapefiles for all classes
+training_poly_list <- lapply(1:class_count, function(i) {
+  filepaths <- list.files(training_paths[[i]], pattern = ".shp$", full.names = TRUE)
+  read_and_merge_training_polygons(filepaths, i)
+})
+training_poly_list
+
+# Merge all classes into one sf object
+training_poly <- do.call(rbind, training_poly_list)
+
+# Add row index for identification
+training_poly <- training_poly %>% mutate(index = rownames(.))
+
+# Validate geometries
+training_poly <- training_poly[st_is_valid(training_poly), ]
+
+#########################################
+### Step 2. Training/Validation Split ###
+#########################################
+
+# read dronestack raster
+drone_stack <- rast(drone_stack_path)
+
+# Select polygons to use for calibration, splits each class by cal/val split
+calibration_set <- training_poly %>%
+  group_by(class) %>%
+  slice_sample(prop = cal_val_split, replace = FALSE) %>%
+  ungroup() %>% 
+  mutate(ID = seq_len(nrow(.)))
+st_write(training_poly, file.path(calval_shp_path_out, "calibration.shp"), append = FALSE)
+
+# Select the remaining validation (test data)
+validation_set <- training_poly %>%
+  filter(!st_equals(., calibration_set, sparse = FALSE) %>% rowSums()) %>% 
+  mutate(ID = seq_len(nrow(.)))
+st_write(training_poly, file.path(calval_shp_path_out, "validation.shp"), append = FALSE)
+
+#extract pixels from calibration polygon subset and join them to the polygon that they were extracted from
+extracted_cal_values <- terra::extract(drone_stack, calibration_set, xy= TRUE)#, bind = TRUE)#, sp = TRUE)# %>% na.omit(extracted_cal_values)
+extracted_cal_values <- extracted_cal_values  %>% left_join(calibration_set, by = "ID", select(ID))
+
+#get the number of pixels in each class to report in ModelRuns.csv
+pixel_counts <- sapply(1:class_count, function(i) {
+  sum(extracted_cal_values$class == i, na.rm = TRUE)
+})
+
+#extract pixels from validation polygon subset
+extracted_val_values <- terra::extract(drone_stack, validation_set, xy= TRUE)# %>% na.omit(extracted_val_values)
+extracted_val_values <- extracted_val_values  %>% left_join(validation_set, by = "ID", select(ID))
+table(extracted_val_values$class)
+
+####################################
+### Step 3. Creation of RF Model ###
+####################################
+
+#Making the class column a factor
+extracted_cal_values$class <- factor(extracted_cal_values$class)
+extracted_val_values$class <- factor(extracted_val_values$class)
+
+# Define predictor variables for modeling
+x_data <- extracted_cal_values[1:15]
+x_data <- subset(x_data, select= -c(ID,alpha,x,y))
+x_data
+#define dependent variable for modeling
+y_data <- extracted_cal_values$class
+y_data <- as.factor(y_data)
+
+y_data
+
+# Line to show corellation between input data
+cor(x_data)
+
+#set seed to be reproducible 
+set.seed(0)
+
+#train model
+rf_classifier <- randomForest(x=x_data, y=y_data, mtry=num_mtry, type = 'response', ntree=num_trees, importance=TRUE, keep.forest=TRUE)
+
+#view the model
+rf_classifier
+plot(rf_classifier)
+
+#get the out of bag error from the model, to add to ModelRuns.csv
+OOB_error <- rf_classifier$err.rate[nrow(rf_classifier$err.rate), "OOB"]
+OOB_error
+
+#line to plot the importance of each variable 
+importance(rf_classifier)
+varImpPlot(rf_classifier, main = 'Band Importance')
+
+#################################################################
+### Step 4. Classifying Camblin Ranch Drone Map with RF Model ###
+#################################################################
+
+#creating file name for output tif
+output_filename <- file.path(output_foldername, paste0("Run",folder_count,'_', current_date, ".tif"))
+output_filename
+
+drone_stack[is.na(drone_stack)] <- 0
+
+# Running RF on the drone stack
+map <- predict(drone_stack, type='response', rf_classifier, filename=output_filename, format="GTiff", overwrite=TRUE)
+plot(map)
+############################################################
+### Step 5. Accuracy assement using ocular training data ###
+############################################################
+
+# Defining Calibration and Validation shapefiles
+validation <- vect(extracted_val_values[, c("class", "x", "y")],geom = c("x", "y"))
+calibration <- vect(extracted_cal_values[, c("class", "x", "y")], geom = c("x", "y"))
+
+# Extracting pixel values from the classified map using the training sample validation points
+validation$reference <- as.factor(validation$class)
+validation$reference
+prediction <- terra::extract(map, validation)
+prediction
+# adding the predicted pixel values to the df with the known pixel values (from training polygons)
+validation$prediction <- as.factor(prediction$class)
+
+# Creating a confusion matrix using the caret package
+cm <- confusionMatrix(as.factor(validation$prediction), as.factor(validation$reference))
+print(cm)
+
+# Getting model metrics to be reported in ModelRuns.csv
+OA <- cm$overall["Accuracy"]
+kappa <- cm$overall["Kappa"]
+
+# making output from caret confusion matrix able ot be plotted
+hm <- as.data.frame(as.table(cm))
+hm
+hm$Prediction <- factor(hm$Prediction, levels = 1:class_count, labels = class_names)
+hm$Reference <- factor(hm$Reference, levels = 1:class_count, labels = class_names)
+
+# Dynamic class labeling for plot
+plot <- ggplot(hm, aes(x = Prediction, y = Reference, fill = Freq)) +
+  geom_tile() +
+  theme_bw() +
+  coord_equal() +
+  scale_fill_distiller(palette = "Greens", direction = 1) +
+  guides(fill = "none") +
+  geom_text(aes(label = Freq), color = "black", size = 10) +
+  scale_x_discrete(limits = class_names, labels = class_names) +
+  scale_y_discrete(limits = class_names, labels = class_names) +
+  # Text size adjustments (optional)
+  theme(
+    axis.text.x = element_text(size = 20, angle = 45, hjust = 1),
+    axis.text.y = element_text(size = 20),
+    axis.title.x = element_text(size = 30),
+    axis.title.y = element_text(size = 30)
+  )
+
+# Display the plot
+plot
+   
+#save plot to output folder
+ggsave(file.path(output_foldername, paste0("ConfusionMatrix_", folder_count, '_', current_date, ".png")), width = 15, height = 8)
+
+
+########################################################
+### Step 5: Write all model metrics to ModelRuns.csv ###
+########################################################
+
+# Create the data frame row for this model run
+new_row <- data.frame(
+  Run_num = folder_count,  # Assuming `folder_count` is defined earlier
+  mtry = num_mtry,         # Assuming `num_mtry` is defined earlier
+  trees = num_trees,       # Assuming `num_trees` is defined earlier
+  calval_split = cal_val_split,  # Assuming `cal_val_split` is defined earlier
+  OverallAccuracy = OA,    # Assuming `OA` is defined earlier
+  Kappa = kappa,           # Assuming `kappa` is defined earlier
+  OOB = OOB_error          # Assuming `OOB_error` is defined earlier
+)
+
+# Add pixel counts for each class dynamically to the row
+for (i in 1:class_count) {
+  new_row[[paste0(class_names[i], "_pix")]] <- pixel_counts[i]
+}
+new_row
+
+# Write Row to table
+write.table(new_row, modelruns_path, row.names = FALSE, col.names = FALSE, sep = ",", append = TRUE)
+
+
